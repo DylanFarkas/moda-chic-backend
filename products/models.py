ae0fa3656983d630347daef2bfa0ed97c5fb3739
Dylan Farkas <dylan.farkas@correounivalle.edu.co>
2025-04-15 18:38:42 -0500

    feat: Models Created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    material = models.CharField(max_length=100)
+    size = models.CharField(max_length=20)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
